3a2b1bc2ac6d0a50767bb6cb32245bcca2128977
Mike Kelly <mk.liffey@proton.me>
2026-07-06 23:32:57 +0100

    Migrate to Spring 4, and tests for postgres

--- server/app/src/test/java/io/github/liffeymike/taskmanager/FlywayMigrationTest.java
+++ server/app/src/test/java/io/github/liffeymike/taskmanager/FlywayMigrationTest.java
@@ -6,18 +6,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
-class FlywayMigrationTest {
-
-  @Container
-  @ServiceConnection
-  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+class FlywayMigrationTest extends AbstractIntegrationTest {
 
   @Autowired
   JdbcTemplate jdbcTemplate;
--- server/app/src/test/java/io/github/liffeymike/taskmanager/PingIntegrationTest.java
+++ server/app/src/test/java/io/github/liffeymike/taskmanager/PingIntegrationTest.java
@@ -2,13 +2,13 @@ package io.github.liffeymike.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
+import org.springframework.boot.graphql.test.autoconfigure.tester.AutoConfigureHttpGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureHttpGraphQlTester
-class PingIntegrationTest {
+class PingIntegrationTest extends AbstractIntegrationTest {
 
   @Autowired
   HttpGraphQlTester graphQlTester;
